2e23224e5dce20bf08d41e115934353b56ec1acd
Tuyisenge Jordan <jordanale.112@gmail.com>
2025-01-19 12:01:01 +0200

    added resident class

--- app.py
+++ app.py
@@ -1,4 +1,26 @@
 from flask import Flask
 app = Flask(__name__)
 
+class Id:
+    name = ""
+    email = ""
+    pin = ""
+    roomNo = 0
+    roomType = ""
+    expenditures = 0
+    myOrderedFoods = []
+
+    def __init__(self, nameR, emailR, pinR, no, typeRoom, expen):
+        self.name = nameR
+        self.email = emailR
+        self.pin = pinR
+        self.roomNo = no
+        self.roomType = typeRoom
+        self.expenditures = expen
+        self.myOrderedFoods = []
+
+    def addOrderedFood(self, name, quantity, price):
+        orderfood = OrderedFoods(name, int(quantity), int(price))
+        self.myOrderedFoods.append(orderfood)
+
 @app.route("/")
